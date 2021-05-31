c94fcb2f0bdc708158870299f46d045439242f69
= <=>
2021-06-01 01:08:57 +0700

    update

---
+++ Lab05/OrtherProject/NoGarbage.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class NoGarbage() {
+    public static void main(Strings[] args)throws FileNotFoundException {
+        String url="";
+        FileInputStream fileInputStream=null;
+        BufferedReader bufferedReader=null;
+        StringBuffer sb=new StringBuffer();
+        long start=System.currentTimeMillis();
+        try{
+            
+        }        
+    }
+    
+}
